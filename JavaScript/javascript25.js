ea3b66ef0fe9b77089e410fb7ba1993887701547
UserRahulrg <newrahulgautam@gmail.com>
2024-07-24 23:38:44 +0530

    Update26.1

---
+++ JavaScript/javascript25.js
@@ -0,0 +1,41 @@
+var gf = "Kirti";
+console.log(gf);
+
+var gf = "Anamika";
+console.log(gf);
+
+
+let gff = "Niharika";
+console.log(gff);
+
+
+gff = "Anita";
+console.log(gff);
+
+
+const gfff = "Prerna";
+console.log(gfff);
+
+gfff = "Lilavati";
+console.log(gfff);
+
+
+
+
+
+
+
+
+console.log();
+console.log();
+
+
+
+
+
+
+
+
+
+
+
