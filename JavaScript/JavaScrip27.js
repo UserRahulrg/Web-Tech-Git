37a7a26cfbd995e815aef4b755164b2c1651e5b0
UserRahulrg <newrahulgautam@gmail.com>
2024-07-26 11:59:45 +0530

    Update28.1

---
+++ JavaScript/JavaScrip27.js
@@ -0,0 +1,32 @@
+var a = 10;
+let b = 2;
+const c = 3;
+
+{
+    var a = 10;
+    let b = 20;
+    const c = 30;
+
+    console.log(a);
+    console.log(b);
+    console.log(c);
+
+    alert(c);
+
+    console.log("Hello World!!");
+
+
+}
+
+if( typeof document !== "undefined"){
+    console.log('document module exist')
+}
+else{
+    console.log('document not exist')
+}
+
+var_arrcustomer = [];
+
+
+
+
