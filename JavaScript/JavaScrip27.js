546c839ed33150204ede1d6cb3dc330bd9046f66
UserRahulrg <newrahulgautam@gmail.com>
2024-07-26 13:20:13 +0530

    Update28.5

--- JavaScript/JavaScrip27.js
+++ JavaScript/JavaScrip27.js
@@ -11,7 +11,7 @@ const c = 3;
     console.log(b);
     console.log(c);
 
-    alert(c);
+    // alert(c);
 
     console.log("Hello World!!");
 
@@ -25,8 +25,65 @@ else{
     console.log('document not exist')
 }
 
-var_arrcustomer = [];
+var var_arrcustomer = [];
+var id = document.getElementById("userid");
+var name = document.getElementById("name");
+var mobileno = document.getElementById("mobileno");
+var email = document.getElementById("email");
+var image = document.getElementById("photo");
+
+function btnaddclick(){
+
+    let id2 = parseInt(valueOf(id));
+    let name = name.value;
+    let mobileno = parseInt(value.mobileno);
+    let id = parseInt(value.id);
+    let email = email.value;
+
+    var reader = new reader();
+
+    reader.onload = function imageload(){
+
+        var image = reader.result;
+
+        adduser(id,name,mobileno,email,image);
+        reader.readAsDataUrl(image.files[0]);
+        alert("Customer added Successfully!!");
+
+    }
+
+    function adduser(var_id,var_name,var_mobileno,var_email,var_image){
+
+        let array = {id:var_id,name:var_name,mobileno:var_mobileno,email:var_email,image:var_image};
+
+        var_arrcustomer(push.array);
+
+        addcustomerintable(array);
+
+    }
+
+    
+    function addcustomerintable(array){
+
+        var vartabletr = document.createElement('tr');
+        vartabletr.setAttribute('id','tr_',array.id.toString());
+
+        var vartdremove = document.createElement('td');
+        var varbtnremove = document.createElement('button');
+        var vartextremove = document.createElement('Remove');
+
+        varbtnremove.appendChild(vartextremove);
+        varbtnremove.appendChild(vartdremove);
+        vartdremove.appendChild(vartabletr);
 
 
 
+
+
+
+
+
+    }
+
+}
 
