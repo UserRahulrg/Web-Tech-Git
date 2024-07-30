7b106bf41981abf09e0e803d448e1c17fe2a943c
UserRahulrg <newrahulgautam@gmail.com>
2024-07-30 23:59:18 +0530

    update28.3

--- JavaScript/JavaScrip27.js
+++ JavaScript/JavaScrip27.js
@@ -1,60 +1,89 @@
 
 var var_arrcustomer = [];
-var var_id = document.getElementById("userid");
-var var_name = document.getElementById("name");
-var var_mobileno = document.getElementById("mobileno");
-var var_email = document.getElementById("email");
-var var_image = document.getElementById("image");
-var var_table = document.getElementById('customer');
+var var_id = document.getElementById('userid');
+var var_name = document.getElementById('name');
+var var_mobileno = document.getElementById('mobileno');
+var var_email = document.getElementById('email');
+var var_image = document.getElementById('image');
+var var_table = document.getElementById('customertable');
 var heading = document.getElementById('headingtime');
-var image;
+
 
 function btnaddclick(){
-    alert("Entered in a Loop!!");
+    
     let id = parseInt(var_id.value);
     let name = var_name.value;
-    let mobileno = parseFloat(var_mobileno.value);
+    let mobileno = parseInt(var_mobileno.value);
     let email = var_email.value;
-    // let image = value.var_image;
+   
 
     var reader = new FileReader();
     reader.onload=function(){
         var image = reader.result;
+        add_customer(id,name,mobileno,email,image);
     }
-
-    let num1 = 10;
-    let num2 = 20;
-
-    let sum = num1 + num2
-
-    add_customer(id,name,mobileno,email,image);
-    alert("Entered in a function!! outside loop"+id+name+mobileno+email+image);
-
+    
     reader.readAsDataURL(var_image.files[0]);
+ 
+
+    
     alert("Customer Data Added Successfully");
 }
 
 function add_customer(var_var_id,var_var_name,var_var_mobileno,var_var_email,var_var_image){
-
-    alert("Entered in add_customer function "+var_var_id,var_var_name,var_var_mobileno,var_var_email,var_var_image);
+   
 
     let objcust = {id:var_var_id,name:var_var_name,mobileno:var_var_mobileno,email:var_var_email,image:var_var_image};
     var_arrcustomer.push(objcust);
-    alert(var_arrcustomer.id);
     addCustomerInTable(objcust);
-
 
 }
 
-function addCustomerInTable(var_objcust){
+function addCustomerInTable(objcust){
 
-    alert("Entered in addCustomerInTable function!!");
+   
 
     var varTableTr = document.createElement('tr');
-    //varTableTr.setAttribute('id','tr_'+objcust.id.ToString());
+    varTableTr.setAttribute('id','tr_'+objcust.id.ToString());
+   
+    var varTdRemove = document.createElement('td');
+    var varBtnRemove = document.createElement('button');
+    var varTextRemove = document.createTextNode('Remove');
+    
+    varBtnRemove.appendChild(varTextRemove);
+    varTdRemove.appendChild(varBtnRemove);
+    varTableTr.appendChild(varTdRemove);
 
-    //var_table.appendChild(varTableTr);
-    alert("JavaScript program Completed!!");
+    var varTdId = document.createElement('td');
+    var varTextId = document.createTextNode(objcust.id);
+    varTdId.appendChild(varTextId);
+    varTableTr.appendChild(varTdId);
+
+    var varTdName = document.createElement('td');
+    var varTextName = document.createTextNode(objcust.name);
+    varTdName.appendChild(varTextName);
+    varTableTr.appendChild(varTdName);
+
+    var varTdMobileNo = document.createElement('td');
+    var varTextMobileNo = document.createTextNode(objcust.mobileno);
+    varTdMobileNo.appendChild(varTextMobileNo);
+    varTableTr.appendChild(varTdMobileNo);
+
+    var varTdEmail = document.createElement('td');
+    var varTextEmail = document.createTextNode(objcust.email);
+    varTdEmail.appendChild(varTextEmail);
+    varTableTr.appendChild(varTdEmail);
+
+    var varTdImage = document.createElement('td');
+    var varTdImageData1 = document.createElement('img');
+    varTdImageData1.setAttribute('src',objcust.image);
+    varTdImageData1.setAttribute('width','90px');
+    
+    varTdImage.appendChild(varTdImageData1);
+    varTableTr.appendChild(varTdImage);
+
+    var_table.appendChild(varTableTr);
+
 }
 
 
