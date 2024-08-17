7b7e6f8e2455e4a38af12ee28420ead864b4b20b
UserRahulrg <newrahulgautam@gmail.com>
2024-08-18 01:02:42 +0530

    Update8.18.1

---
+++ JavaScript/Day32/javascriptday32.js
@@ -0,0 +1,55 @@
+let table = document.getElementsByClassName("createtable");
+
+function clickclick(){
+    
+    alert("hello world!!");
+    let tabletable = document.createElement("table");
+    let tableteabletr = document.createElement("tr");
+    let tabltableth = document.createElement("th");
+
+    tableteabletr.appendChild(tabltableth);
+    tabletable.appendChild(tableteabletr);
+    table.appendChild(tabletable);
+
+    alert(table);
+
+}
+
+function clickclickclick(){
+    var tabletable = document.getElementById("table01");
+    let tableteabletr = document.createElement("tr");
+    var row = tabletable.insertRow(1);
+    var cel1 = row.insertCell(0);
+
+
+    var cel2 =row.insertCell(0);
+    cel1.innerHTML = "NewCell1";
+    cel2.innerHTML = "NewCell2";
+
+}
+
+function createTable(){
+    let table = document.createElement("table");
+    table.setAttribute('border','4');
+
+    let row = table.insertRow();
+
+    let cell1 = row.insertCell();
+    let cell2 = row.insertCell();
+
+    var name = document.getElementById('name');
+    var color = document.getElementById('color');
+    cell1.textContent = name.value;
+    cell2.textContent = color.value;
+
+    document.body.appendChild(table);
+}
+
+
+
+
+
+
+
+
+
---
+++ Php/PaymentInterfaceIntegration/checkout.php
@@ -0,0 +1,30 @@
+<html>
+    <header>
+
+    </header>
+
+    <body>
+        <head>
+
+        </head>
+        <form action="payscript.php" method="post">
+            <div class="formcheckout">
+            <label for="fname">Full Name</label>
+            <input type="text" id="name" name="name">
+            <label for="hidden">none</label>
+            <input type="hidden" id="name" name="name">
+            <label for="hidden">none</label>
+            <input type="hidden" id="name" name="name">
+            <label for="adr">Address</label>
+            <input type="text" id="name" name="name">
+            <label for="">None</label>
+            <input type="hidden" id="name" name="name">
+            </div>
+            <input type="submit" value="Pay Now" class="btn">
+        </form>
+
+        <footer>
+
+        </footer>
+    </body>
+</html>
