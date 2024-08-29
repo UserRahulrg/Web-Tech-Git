267cd003f718b84621ba94082b26299543f6c1d7
UserRahulrg <newrahulgautam@gmail.com>
2024-08-29 16:37:00 +0530

    Update8.29.1

---
+++ JavaScript/Day-37/javascript37.js
@@ -0,0 +1,61 @@
+
+function PrimeNumberOrNot(){
+
+    prompt("Enter a number check for Prime Number!!");
+}
+
+function NumberCountADigit(){
+    prompt("Enter a number count digit for :");
+
+    
+
+}
+
+function AcceptANumberAndReverseANumber(){
+    prompt("Enter a Number check Accept a Number and Reverse a Number ");
+
+    var number = document.getElementById("button1");
+    var number = parseInt(number);
+    prompt(number);
+
+    var i = 0;
+    for(i < number)
+        {
+        if(number > 10){
+           
+        }
+    }
+}
+
+function Parent()
+{
+    function Child(){
+        console.log("Child Function!!");
+    }
+}
+
+function Parentparent(){
+
+    function Child(){
+        prompt("Child Function");
+    }
+
+    return Child;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
