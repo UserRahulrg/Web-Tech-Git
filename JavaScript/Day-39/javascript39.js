f97a8004aa3676368d5cdb80e83ea7a102c31807
UserRahulrg <newrahulgautam@gmail.com>
2024-09-03 11:53:11 +0530

    update09.03.03

---
+++ JavaScript/Day-39/javascript39.js
@@ -0,0 +1,15 @@
+let value = firstClass();
+console.log(value);
+
+function firstClass(){
+    let valueofprompt=prompt("This is First class function!");
+    return valueofprompt;
+}
+
+
+
+
+
+
+
+
