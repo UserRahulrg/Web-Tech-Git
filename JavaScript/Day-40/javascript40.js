641fc11bef8f622b8b922e24f167202045916a72
UserRahulrg <newrahulgautam@gmail.com>
2024-09-05 08:37:05 +0530

    Update09.05.01

---
+++ JavaScript/Day-40/javascript40.js
@@ -0,0 +1,25 @@
+const arr = [10,20,30,40,50,60,null,undefined];
+
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[6]);
+console.log(arr[7]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
