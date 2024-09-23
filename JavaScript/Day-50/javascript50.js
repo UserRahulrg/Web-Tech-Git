ba1a1b78021bb667012d237c8319cd876062ff6e
UserRahulrg <newrahulgautam@gmail.com>
2024-09-23 12:11:58 +0530

    Update09.23.01

--- JavaScript/Day-50/javascript50.js
+++ JavaScript/Day-50/javascript50.js
@@ -24,3 +24,31 @@ window.addEventListener("keydown",(e)=> {
     box.style.top=tSide+"px"
 
 })
+
+const box = document.getElementById("box");
+let lSide = 100;
+let tSide = 100;
+
+window.addEventListener("keydown",(e)=> {
+    let move =50;
+    switch(e.key){
+        case "w":
+            tSide =tSide-move;
+            break;
+        case "s":
+            tSide =tSide-move;
+            break;
+        case "a":
+            tSide =tSide-move;
+            break;
+        case "d":
+            tSide =tSide-move;
+            break;
+        default:
+            break;
+    }
+    box.style.left=lSide+"px"
+    box.style.top=tSide+"px"
+
+})
+
