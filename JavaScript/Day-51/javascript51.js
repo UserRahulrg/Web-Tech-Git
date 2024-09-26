3c9dbc45e06f326a8a79e294ebf7793983a6f87f
UserRahulrg <newrahulgautam@gmail.com>
2024-09-26 09:51:27 +0530

    Update09.26.01

---
+++ JavaScript/Day-51/javascript51.js
@@ -0,0 +1,37 @@
+const btnelem1 = document.getElementById("btn1")
+const btnelem2 = document.getElementById("btn2")
+
+btnelem1.addEventListener("click",()=>{
+    window.open("https://www.google.com/","_blank","width:480,height:800")
+})
+
+btnelem2.addEventListener("click",()=>{
+    windowName.close()
+})
+
+console.log(window.Navigator)
+
+const btn = document.getElementById("copy")
+
+btn.addEventListener("click",()=>{
+
+    const p = document.querySelector("p");
+    const content = p.textContent;
+    navigator.clipboard.writeText(content);
+})
+
+const pasteelem1 = document.getElementById("paste")
+
+pasteelem1.addEventListener("paste",(e)=>{
+
+    e.preventDefault();
+})
+
+
+
+
+
+
+
+
+
