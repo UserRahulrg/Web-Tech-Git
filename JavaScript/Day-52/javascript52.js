c010c6671b6bc15002a6c7ea91cd6eea407cdf35
UserRahulrg <newrahulgautam@gmail.com>
2024-09-29 10:42:17 +0530

    Update09.29.01

--- JavaScript/Day-52/javascript52.js
+++ JavaScript/Day-52/javascript52.js
@@ -25,4 +25,39 @@ setInterval(()=>{
 },1000)
 
 
+//took ref of Button 
+
+/*
+const btncopy = document.getElementById("copy")
+const btnblock = document.getElementById(".code")
+
+btncopy.addEventListener("click",()=>{
+    const code = document.querySelector(".code")
+    const content = code.textContent;
+
+    navigator.clipboard.writeText(content)
+
+    console.log(content)
+
+    const overlay = document.createElement("div")
+    overlay.classList.add("overlay")
+
+    const overlayMsg = document.createElement("div")
+
+    overlayMsg.textContent='Text Copied to Clipboard!!'
+    overlayMsg.classList.add("overlay-message")
+
+   overlayMsg.textContent='Text Copied to Clipboard!!'
+    overlayMsg.classList.add("overlay-message")
+    
+})
+
+const overlayMsg = document.createElement("div")
+
+overlayMsg.textContent='Text Copied to Clipboard!!'
+overlayMsg.classList.add("overlay-message")
+btnblock.appendChild(overlay)
+
+
+*/
 
