2f725c4e777c8b3109bf01ce23588d83194815b9
UserRahulrg <newrahulgautam@gmail.com>
2024-10-08 09:13:49 +0530

    Update24.10.08.01

--- JavaScript/Day-55/javascript55.js
+++ JavaScript/Day-55/javascript55.js
@@ -1,1 +1,14 @@
+function openPopup(){
+
+    const overlay = document.querySelector(".overlay")
+    overlay.style.display="block";
+    const popup = document.querySelector(".myPop");
+    popup.classList.add("show")
+}
+
+window.addEventListener("load",()=>{
+    setTimeout(openPopup,5000);
+})
+
+
 
---
+++ JavaScript/Day-56/javascript56.js
@@ -0,0 +1,15 @@
+const acc = document.getElementById(".accordionTab");
+
+acc.addEventListener("click",()=>{
+    const cross = document.querySelector('.accord1')
+    cross.classList.toggle("show")
+})
+
+
+
+
+
+
+
+
+
