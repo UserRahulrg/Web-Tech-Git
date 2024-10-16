70944e7c7ab7da66d49cd78da9579cc53bd3ce73
UserRahulrg <newrahulgautam@gmail.com>
2024-10-16 08:19:03 +0530

    Update -m 24.10.16.01

---
+++ Revision/Day-58/Day-59/javascript59sidebar.js
@@ -0,0 +1,6 @@
+var elesidebarnav = document.getElementById('sidebarnav');
+
+var elesidebarnavclick = document.querySelector('nav-bar');
+elesidebarnav.addEventListener("click",()=>{
+    elesidebarnavclick.style.display="flex";
+})
