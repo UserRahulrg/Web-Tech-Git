c7abaa1a61b42ced5a91171c22b8bb717dfe6ac2
UserRahulrg <newrahulgautam@gmail.com>
2024-11-02 18:40:07 +0530

    Update24.11.02.01

---
+++ Revision/Day-58/Day-60/javascript60.js
@@ -0,0 +1,28 @@
+alert("javascript is enabled!")
+
+var x = 100;
+var y = 20;
+
+var sum;
+var subtract;
+var multiply;
+var divide;
+var approx;
+
+sum = x + y;
+alert("sum is :"+sum)
+subtract = x - y;
+alert("subtracted value is :"+subtract)
+multiply = x * y;
+alert("multiplied value is :"+multiply)
+divide = x / y;
+alert("divided value is :"+divide);
+if(x > y){approx = x - y}else{approx = y - x}
+alert("approx value is : "+approx);
+
+
+
+
+
+
+
