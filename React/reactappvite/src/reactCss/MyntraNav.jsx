74bf0c0a228bf07478244acff6ffb8eb295c30a2
UserRahulrg <newrahulgautam@gmail.com>
2025-01-11 23:45:12 +0530

    Update25.01.11.01

--- React/reactappvite/src/reactCss/MyntraNav.jsx
+++ React/reactappvite/src/reactCss/MyntraNav.jsx
@@ -11,14 +11,15 @@ const MyntraNav = ()=>{
             <nav id={styles.nav}>
                 <h4 id = {styles.h4}>Myntra</h4>
                 <ul id={styles.ul}>
-                    <li type="none"><a href="" id={styles.li}>MEN</a></li>
-                    <li type="none"><a href="">WOMEN</a></li>
-                    <li type="none"><a href="">KIDS</a></li>
-                    <li type="none"><a href="">HOME & LIVING</a></li>
-                    <li type="none"><a href="">BEAUTY</a></li>
-                    <li type="none"><a href="">STUDIO</a></li>
-                    <li ><a href=""></a></li>
+                    <li type="none" id={styles.li}><a href="" >MEN</a></li>
+                    <li type="none" id={styles.li}><a href="">WOMEN</a></li>
+                    <li type="none" id={styles.li}><a href="">KIDS</a></li>
+                    <li type="none" id={styles.li}><a href="">HOME & LIVING</a></li>
+                    <li type="none" id={styles.li}><a href="">BEAUTY</a></li>
+                    <li type="none" id={styles.li}><a href="">STUDIO</a></li>
+                    
                 </ul>
+                <h4 id = {styles.h4}>Cart</h4>
             </nav>
             
         </div>
